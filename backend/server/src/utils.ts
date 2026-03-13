04117d022dabdda0cc6bacc27b5dad70bcdfd555
sairamesh-7 <sairamesh.pragada@gmail.com>
2026-03-13 22:18:31 +0530

    Update Cloudflare API URL and authorization key

--- backend/server/src/utils.ts
+++ backend/server/src/utils.ts
@@ -107,11 +107,11 @@ export const generateCode = async ({
   instructions: string;
 }) => {
   return await fetch(
-    "https://api.cloudflare.com/client/v4/accounts/dbcc31144a91550582475baf9bb01af1/ai/run/@cf/meta/llama-3-8b-instruct",
+    "https://api.cloudflare.com/client/v4/accounts/YOUR_ACCOUNT_ID/ai/run/@cf/meta/llama-3-8b-instruct",
     {
       method: "POST",
       headers: {
-        Authorization: "Bearer 0_N7YSPsYKCzQwjngSADN11d3eq1gUCIw9EMmna1",
+        Authorization: "Bearer YOUR_API_KEY",
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
